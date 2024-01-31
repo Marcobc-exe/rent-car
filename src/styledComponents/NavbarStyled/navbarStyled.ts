3d36fab5eca4e45650104d51ec3b2507b5371b1b
Marcobc-exe <marco.developing@gmail.com>
2024-01-31 13:52:45 -0300

    Styles(Navbar): Navbar responsive design was created

--- src/styledComponents/NavbarStyled/navbarStyled.ts
+++ src/styledComponents/NavbarStyled/navbarStyled.ts
@@ -11,6 +11,9 @@ const ContainerNavbar = styled.div`
   align-items: center;
   padding: 25px 80px;
   justify-content: right;
+  @media (max-width: 425px) {
+    padding: 16px 14px;
+  }
 `;
 
 const BtnForm = styled.div<{ $active?: boolean }>`
@@ -25,6 +28,19 @@ const BtnForm = styled.div<{ $active?: boolean }>`
   cursor: pointer;
   height: 40px;
   text-decoration: none;
+
+  @media (max-width: 425px) {
+    background-color: #fff;
+    color: #0c0c0c;
+    font-family: ${(props) => (props.$active ? "Inter Medium" : "Inter Light")};
+    border-radius: 0;
+    font-size: 16px;
+    font-weight: ${(props) => (props.$active ? 600 : 400)};
+    line-height: 24px;
+    letter-spacing: 0.04px;
+    padding: 0;
+    text-decoration-line: ${(props) => (props.$active ? "underline" : "blink")};
+  }
 `;
 
 export { ContainerNavbar, BtnForm };
